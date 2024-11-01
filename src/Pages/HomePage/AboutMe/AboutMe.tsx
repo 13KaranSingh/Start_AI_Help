1df4bceefe6a48248615252b0b6b9f5a1f5c1b2c
SUCHI <svp@udel.edu>
2024-11-01 16:29:58 -0400

    changed layout of about us and founders

--- src/Pages/HomePage/AboutMe/AboutMe.tsx
+++ src/Pages/HomePage/AboutMe/AboutMe.tsx
@@ -3,17 +3,18 @@ import React from 'react';
 
 const AboutMe = () => {
     return (
-        <div className="about-us">
-            <h1>About Us</h1>
-            <p>This quiz was created to help users pick their career field. Add more info Later</p>
-            
-            <h2>Founders</h2>
-            <ul className="founders-list">
-                <li><strong>Karan Singh</strong> - ...</li>
-                <li><strong>Suchi Patel</strong> - ...</li>
-                <li><strong>Alyssa Sanchez</strong> - ...</li>
-                <li><strong>Jasmyn-JayNavarro </strong> - ...</li>
-            </ul>
+        <div className="about-container">
+            <div className="founder-box">
+                <h2>Founders</h2>
+                <div className="founder-square"><strong>Karan Singh</strong></div>
+                <div className="founder-square"><strong>Suchi Patel</strong></div>
+                <div className="founder-square"><strong>Alyssa Sanchez</strong></div>
+                <div className="founder-square"><strong>Jasmyn Navarro</strong></div>
+            </div>
+            <div className="about-us">
+                <h1>About Us</h1>
+                <p>This quiz was created to help users pick their career field. Add more info Later</p>
+            </div>
         </div>
     );
 };
